2445044f3ca1428868a910e3c4d3684908a2e2dc
jayanp <jayanp0202@gmail.com>
2026-07-17 09:59:12 -0500

    upd help url

--- api/entrypoint.go
+++ api/entrypoint.go
@@ -16,7 +16,7 @@ func init() {
 
 	message := orderedmap.New()
 	message.Set("title", "Welcome to the Home Access Center API!")
-	message.Set("message", "This is the home page, visit the documentation at https://homeaccesscenterapi-docs.vercel.app/ for more information on how to use this API.")
+	message.Set("message", "This is the home page, visit the documentation at https://gradient-hac-api-docs.vercel.app/ for more information on how to use this API.")
 	message.Set("routes", []string{"/api/name", "/api/assignments", "/api/info", "/api/averages", "/api/classes", "/api/reportcard", "/api/ipr", "/api/transcript", "/api/rank"})
 
 	home := func(c *gin.Context) { c.JSON(200, message) }
